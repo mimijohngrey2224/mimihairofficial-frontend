0a3fdd840623e4dc12ab44f9a056278906f601ca
MIMi <mimijohngrey@outlook.com>
2026-07-15 15:09:21 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -90,7 +90,7 @@ function Footer() {
 
       {/* Divider */}
       <div className="border-t border-gray-700 mt-8 pt-4 text-center text-sm text-gray-300">
-        © {new Date().getFullYear()} MimiHair. All rights reserved.
+        © {new Date().getFullYear()} Mimihairoffical. All rights reserved.
       </div>
     </footer>
   );
